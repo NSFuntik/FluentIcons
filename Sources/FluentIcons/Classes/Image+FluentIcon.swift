487412991d07249c968e1ae9505fa4f2ca42591f
Dmitry Mikhaylov <nsfuntik@gmail.com>
2024-08-08 05:46:27 +0300

    Правки iOS: 1. В блоке "Аналоги" в карточке товара должен быть зеленый текст (посмотреть в фигме); 2. В блоке "Вы недавно смотрели" в карточке товара должен быть зеленый текст (посмотреть в фигме); 3. Сделать порядок блоков в карточке товара согласно фигме; 4. Адреса аптек не по фигме. Вопрос насколько трудоемко сделать по фигме (в андроиде реализовано как надо); 5. В моих заказах при переходе в конкретный заказ внизу где отображаются товары нет инфы по кол-ву (сколько штук) и еще доп инфы;

--- Sources/FluentIcons/Classes/Image+FluentIcon.swift
+++ Sources/FluentIcons/Classes/Image+FluentIcon.swift
@@ -11,12 +11,14 @@ public extension Image {
   init(fluent: FluentIcon) {
     self.init(fluent.resourceString, bundle: .init(for: FluentIconBundle.self))
   }
-  
+
   init(_ fluent: FluentIcon) {
     self.init(fluent.resourceString, bundle: .init(for: FluentIconBundle.self))
   }
-  // >>>>>>> 2b88aafe (removed Regular suffix)}
 }
-fileprivate class FluentIconBundle: Bundle, @unchecked Sendable { }
+
+// MARK: - FluentIconBundle
+
+private class FluentIconBundle: Bundle, @unchecked Sendable {}
 
 public typealias Icon = FluentIcon
